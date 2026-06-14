aa86973f9dd4d4b2d285d82e28ceb83ce8466849
pratikkuyate <pratikkuyate987@gmail.com>
2026-06-14 21:24:02 +0530

    fix robots file // issue

--- app/robots.js
+++ app/robots.js
@@ -1,5 +1,5 @@
 export default function robots() {
-  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL;
+  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL?.replace(/\/$/, '') || '';
   return {
     rules: [
       {
