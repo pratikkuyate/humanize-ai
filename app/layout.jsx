1e46264c2ec5c81c9fd82289821d635a641d11e6
pratikkuyate <pratikkuyate987@gmail.com>
2026-06-14 18:05:29 +0530

    add GMT code

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata = {
   metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL),
@@ -70,6 +71,18 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body>{children}</body>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-GZEF2PGXC2"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-GZEF2PGXC2');
+        `}
+      </Script>
     </html>
   );
 }
