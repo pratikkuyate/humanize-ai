f1c0372bb7c5363f994b0a0b031c26bce65c4bee
pratikkuyate <pratikkuyate987@gmail.com>
2026-06-16 21:26:59 +0530

    fix robots.txt issue

--- app/robots.js
+++ app/robots.js
@@ -1,14 +1,15 @@
 export default function robots() {
-  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL?.replace(/\/$/, '') || '';
+  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL?.replace(/\/$/, "") || "";
+  const domain = new URL(baseUrl).hostname || "";
   return {
     rules: [
       {
-        userAgent: '*',
-        allow: '/',
-        disallow: '/api/',
+        userAgent: "*",
+        allow: "/",
+        disallow: "/api/",
       },
     ],
     sitemap: `${baseUrl}/sitemap.xml`,
-    host: baseUrl,
+    host: domain,
   };
 }
